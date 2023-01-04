f7f7067b9aac75ac9ce948b6aec60ca6407084fc
raylanef <raylanefernandes@hotmail.com>
2023-01-04 20:29:46 -0300

    add paths in ts.config and modifications in the tests

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,26 +1,20 @@
+import { InMemoryNotificationsRepository } from "@test/repositories/in-memory-notifications-repository";
 import { Notification } from "../entities/notification";
 import { SendNotification } from "./send-notification";
 
-const notifications: Notification[] = [];
-
-const notificationsRepository = {
-    async create(notification: Notification) {
-        notifications.push(notification);
-    }
-};
 
 describe('Send notification', () => {
     it('should be able to send a notification', async () => {
+        const notificationsRepository = new InMemoryNotificationsRepository();
         const sendNotification = new SendNotification(notificationsRepository);
 
-        await sendNotification.execute({
+        const { notification } = await sendNotification.execute({
             content: 'This is a notificatio',
             category: 'social',
             recipientId: 'example-recipient-Id'
         });
 
-        console.log(notifications);
-
-        expect(notifications).toHaveLength(1);
+        expect(notificationsRepository.notifications).toHaveLength(1);
+        expect(notificationsRepository.notifications[0]).toEqual(notification);
     });
 });
