26e91c9a7b542adc00cd690f7389d21aededf8ed
raylanef <raylanefernandes@hotmail.com>
2023-01-04 21:13:15 -0300

    add view model

--- src/infra/http/controllers/notifications.controller.ts
+++ src/infra/http/controllers/notifications.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { SendNotification } from '@application/use-cases/send-notification';
 import { CreateNotificationBody } from '../dtos/create-notification-body';
+import { NotificationViewModel } from '../view-models/notification-view-model';
 
 @Controller('notifications')
 export class NotificationsController {
@@ -15,6 +16,9 @@ export class NotificationsController {
       content,
       category
     });
-    return { notification };
+
+    return { 
+      notification : NotificationViewModel.toHTTP(notification)
+    };
   }
 }
